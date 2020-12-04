8f1eaf205a5cb6b1a8aedb0a0a615972e984971c
Fabrício <53907830+Fabricio-Silva-Cardoso1@users.noreply.github.com>
2020-12-04 17:53:58 -0300

    Add files via upload

--- ClinicWebsite/public/javascripts/backwork/functions.js
+++ ClinicWebsite/public/javascripts/backwork/functions.js
@@ -1,17 +1,31 @@
-function confirmRegister (){
+const { getEnabledCategories } = require("trace_events");
 
+function infos(name,cpf,sexo,data_nasc,senha,telefone,email){
+    this.name = name;
+    this.cpf = cpf;
+    this.sexo = sexo;
+    this.data_nasc = data_nasc;
+    this.senha = senha;
+    this.telefone = telefone;
+    this.email= email;
 }
 
-function cancelRegister (){
-
-
+var sexo = document.getElementsByName('gender');
+for(var i; i<gender.length;i++){
+    if(gender[i].checked){
+        sexo = gender[i].value;
+    }
 }
 
-let map;
+var name = document.getElementsByName('Name');
+var cpf = document.getElementsByName('CPF');
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
+var data_nasc = document.getElementsByName('date_nasc')
+var data = document.getElementsByName('Date');
+var password = document.getElementsByName('Password');
+var tel = document.getElementsByName('Telefone');
+var email = document.getElementsByName('E-mail');
+
+var bd = [
+    new infos(name,cpf,sexo,data_nasc,data,password,tel,email)
+];
