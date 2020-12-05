a64c39397085d5221fd7146bc2630ccb25f96e86
Cesar Marrote Manzano <cesarmarrote@gmail.com>
2020-12-05 10:07:29 -0300

    adding procfile and ibm watson credentials

--- ClinicWebsite/lib/ibmWatsonCredentials.js
+++ ClinicWebsite/lib/ibmWatsonCredentials.js
@@ -4,7 +4,7 @@ const { IamAuthenticator } = require('ibm-watson/auth');
 const assistant = new AssistantV1({
     url: 'https://gateway.watsonplatform.net/assistant/api',
     version: '1.0.0',
-    authenticator: new IamAuthenticator({ apikey: '' })
+    authenticator: new IamAuthenticator({ apikey: '3RtDrhV5JbvkM3Srlnw9gJWVq-J4HBhIt2uipWyXQaXS' })
 });
 
 module.exports = {assistant};
